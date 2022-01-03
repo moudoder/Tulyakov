7ce61025b814576300baef24445947878c8c4a4c
moudoder <39887984+moudoder@users.noreply.github.com>
2022-01-03 17:49:49 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  new WOW().init();
+
+  $(".scrol-to a").on("click", function (event) {
+    event.preventDefault();
+    
+    let id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    top = top - 50;
+    $('html').animate({scrollTop: top}, 1500);
+    
+  });
+
+  $('.stages-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    swipe: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: $('.stages-arrow_prev'),
+    nextArrow: $('.stages-arrow_next'),
+    responsive: [
+        {
+          breakpoint: 720,
+          settings: {
+            slidesToShow: 1,
+            autoplay: false,
+            variableWidth: true,
+            infinite: false,
+            infinite: false,
+          }
+        }
+      ]
+   });
+
+  if ($(window).width() < 992) {
+    $('.practices-row').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      swipe: true,
+      prevArrow: $('.practices-arrow_prev'),
+      nextArrow: $('.practices-arrow_next'),
+     });
+  }
+
+  $('.questions-block').on('click', function() {
+    $(this).toggleClass('questions-line-active');
+  })
+
+})
